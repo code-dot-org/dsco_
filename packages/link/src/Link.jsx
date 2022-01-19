b1399c8fd92ebdb13d0eb971dd43f12701dfaf8d
Madelyn Kasula <maddie.kasula@code.org>
2022-01-19 10:00:09 -0800

    feat(link): add prop-types and fix eslint errors

--- packages/link/src/Link.jsx
+++ packages/link/src/Link.jsx
@@ -1,6 +1,11 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-// TODO: add prop-types
 
 export default function Link(props) {
   return <a href={props.href}>{props.text}</a>;
 }
+
+Link.propTypes = {
+  href: PropTypes.string.isRequired,
+  text: PropTypes.text.isRequired,
+};
