498654c33d097db3e1d3a6681e68981054cf00a0
Madelyn Kasula <maddie.kasula@code.org>
2022-02-05 23:22:30 -0800

    feat(link): implement initial component API

--- packages/link/lib/Link.jsx
+++ packages/link/lib/Link.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import * as styles from './link.module.scss';
 
-export default function Link(props) {
+export default function Link({
+  children,
+  className,
+  external,
+  href,
+  id,
+  onClick,
+  openInNewTab,
+  weight,
+}) {
   return (
-    <a href={props.href} className={styles.link}>
-      {props.text}
+    <a
+      className={classnames(styles.link, styles[weight], className)}
+      href={href}
+      id={id}
+      onClick={onClick}
+      target={openInNewTab ? '_blank' : undefined}
+      rel={openInNewTab && external ? 'noopener noreferrer' : undefined}
+    >
+      {children}
     </a>
   );
 }
 
 Link.displayName = 'Link';
 
-Link.propTypes = {
+export const linkPropTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  external: PropTypes.bool,
   href: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  onClick: PropTypes.func,
+  openInNewTab: PropTypes.bool,
+  weight: PropTypes.oneOf(['regular', 'medium', 'bold']),
+};
+Link.propTypes = linkPropTypes;
+
+Link.defaultProps = {
+  children: undefined,
+  className: undefined,
+  external: false,
+  id: undefined,
+  onClick: () => {},
+  openInNewTab: false,
+  weight: 'regular',
 };
