e703eef5fb0aa1b99340b0d763fc278c305b68fd
Madelyn Kasula <maddie.kasula@code.org>
2022-02-06 11:14:48 -0800

    chore(link): add actions and all default props to stories

--- packages/link/__stories__/Link.stories.js
+++ packages/link/__stories__/Link.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from '../lib/Link';
 
 export default {
+  argTypes: {onClick: {action: 'onClick'}},
   title: 'Link',
 };
 
--- packages/link/__stories__/TextLink.stories.js
+++ packages/link/__stories__/TextLink.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextLink from '../lib/TextLink';
 
 export default {
+  argTypes: {onClick: {action: 'onClick'}},
   title: 'TextLink',
 };
 
@@ -9,9 +10,11 @@ const Template = (args) => <TextLink {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
+  external: true,
   href: 'https://studio.code.org',
   icon: <i className="fa fa-external-link-alt" />,
   iconBefore: false,
+  openInNewTab: true,
   text: 'Open Code Studio',
   weight: 'regular',
 };
