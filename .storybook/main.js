19c38b435ade1b214e9d68e233bc23dc36b1e907
Elijah <me@elijahs.space>
2022-02-08 11:37:53 -0700

    chore(root): add additional storybook plugins

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,11 @@ const webpackConfig = require('../webpack.config');
 module.exports = {
   stories: ['../packages/**/__stories__/*.stories.js'],
   addons: [
+    '@storybook/addon-a11y',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    'storybook-addon-performance/register',
+    '@whitespace/storybook-addon-html',
     'storybook-addon-rtl',
   ],
   framework: '@storybook/react',
