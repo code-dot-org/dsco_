96d5295a0fa7e8f187561955e4668f2877018583
Madelyn Kasula <maddie.kasula@code.org>
2022-02-28 12:47:24 -0800

    chore(root): use expanded controls to include docs in 'controls' addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,6 +6,7 @@ initializeRTL();
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
